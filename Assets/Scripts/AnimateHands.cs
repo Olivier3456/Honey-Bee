82432492a199d0c46070fd718ff4c074d748c8de
Olivier3456 <olivier.larrey@gmail.com>
2023-01-04 12:44:20 +0100

    Ajout et config des packages XR

---
+++ Assets/Scripts/AnimateHands.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHands : MonoBehaviour
+{
+
+    [SerializeField] private InputActionProperty _pinchAction;
+
+    [SerializeField] private InputActionReference _gripAction;
+
+    [SerializeField] private Animator _animator;
+    
+    void Start()
+    {
+        
+    }
+        
+    void Update()
+    {
+
+        float triggerVal = _pinchAction.action.ReadValue<float>();
+        _animator.SetFloat("Trigger", triggerVal);
+
+        float gripVal = _gripAction.action.ReadValue<float>();
+        _animator.SetFloat("Grip",gripVal);
+
+    }
+    
+}
